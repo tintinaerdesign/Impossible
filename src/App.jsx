659c1bbbffd1a8ff50288098bc39009169847935
Impossible <toeng0966945736@gmail.com>
2026-05-31 12:47:37 +0700

    one more time

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import "./index.css";
-import { Routes, Route } from "react-router-dom";
-import StartD from "./pages/StartD"; // ดึงไฟล์ที่เราแก้ร่วมกันล่าสุดมาใช้
+import StartD from "./pages/StartD";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-[#0b0a0f] text-white font-sans">
-      <Routes>
-        {/* เปิดหน้าแรกมาปุ๊บ (/) เจอโปรเจกต์หลักของคุณทันทีไม่ต้องเข้าลิงก์ย่อย */}
-        <Route path="/" element={<StartD />} />
-
-        {/* แสตนด์บายลิงก์นี้ไว้ด้วย เผื่อกดเมนูบาร์ด้านบน */}
-        <Route path="/start-d" element={<StartD />} />
-      </Routes>
+      {/* แก้ไขบรรทัดด้านล่างนี้ เติม / ไว้หน้าเครื่องหมาย > */}
+      <StartD />
     </div>
   );
 }
