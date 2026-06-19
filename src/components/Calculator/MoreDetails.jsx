eb1f7558f6b50bd82eebc50c3f7cbdcb7e3f9e6b
Impossible <toeng0966945736@gmail.com>
2026-06-20 04:03:51 +0700

    main

--- src/components/Calculator/MoreDetails.jsx
+++ src/components/Calculator/MoreDetails.jsx
@@ -109,36 +109,36 @@ export default function MoreDetails({
               />
 
               {/* Stats */}
-              <div className="grid md:grid-cols-4 gap-4">
-                <div className="rounded-3xl p-6 bg-white/5 border border-white/5">
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                <div className="rounded-3xl p-6 sm:p-1 bg-white/5 border border-white/5">
                   <p className="text-zinc-500 text-sm">Future Value</p>
 
-                  <h2 className="text-3xl font-bold text-[#F97316] mt-2">
+                  <h2 className="text-3xl sm:text-xl font-bold text-[#F97316] mt-2">
                     ${Math.round(fvResult)?.toLocaleString(undefined)}
                   </h2>
                 </div>
-                <div className="rounded-3xl p-6 bg-white/5 border border-white/5">
+                <div className="rounded-3xl p-6 sm:p-1 bg-white/5 border border-white/5">
                   <p className="text-zinc-500 text-sm">Total Invested</p>
 
-                  <h2 className="text-3xl font-bold mt-2">
+                  <h2 className="text-3xl sm:text-xl font-bold mt-2">
                     ${Math.round(principal)?.toLocaleString(undefined)}
                   </h2>
                 </div>
 
-                <div className="rounded-3xl p-6 bg-white/5 border border-white/5">
+                <div className="rounded-3xl p-6 sm:p-1 bg-white/5 border border-white/5">
                   <p className="text-zinc-500 text-sm">Purchasing Power</p>
 
-                  <h2 className="text-3xl font-bold mt-2 text-[#EC4899]">
+                  <h2 className="text-3xl sm:text-xl font-bold mt-2 text-[#EC4899]">
                     ${Math.round(adjustedPurchasingPower)?.toLocaleString()}
                   </h2>
                 </div>
 
-                <div className="rounded-3xl p-6 bg-white/5 border border-white/5">
+                <div className="rounded-3xl p-6 sm:p-1 bg-white/5 border border-white/5">
                   <p className="text-zinc-500 text-sm">
                     Inflation Loss ({savingPeriod} years)
                   </p>
 
-                  <h2 className="text-3xl font-bold mt-2 text-red-600">
+                  <h2 className="text-3xl sm:text-xl font-bold mt-2 text-red-600">
                     -{Math.abs(totalPowerLossPercent).toFixed(1)}%
                   </h2>
                 </div>
